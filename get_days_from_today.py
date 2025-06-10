61ff93ad5da87d4cfd86d6625b1bcbab7a1b0fb1
Kateryna Karpova <katyakarpova@gmail.com>
2025-06-10 22:57:10 +0200

    removed prints

--- get_days_from_today.py
+++ get_days_from_today.py
@@ -2,12 +2,7 @@ import datetime
 
 def get_days_from_today(date):
     today = datetime.date.today()
-    print("Today>>>", today)
-    
     target_date = datetime.datetime.strptime(date, '%Y-%m-%d').date()
-    print("Target Date>>>", target_date)
-    
     delta = today - target_date
-    return delta.days
 
-print(get_days_from_today('2025-10-01'))
+    return delta.days
